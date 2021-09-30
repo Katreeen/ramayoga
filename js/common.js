b6ebd00abc183993f7a6b70bed35ed744b47f39e
Katreen <zolotaevaekaterina@gmail.com>
2021-09-30 22:56:25 +0400

    first commit

---
+++ js/common.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+
+$('.main_slider').owlCarousel({
+  loop:true,
+  nav:true,
+  items:1,
+  dots:false,
+  slideBy:1,
+  smartSpeed:1000,
+  fluidSpeed:1000,
+  mouseDrag:true,
+  touchDrag:true,
+})
+
+$('.sets_item_gallery').owlCarousel({
+  loop:true,
+  nav:false,
+  items:1,
+  dots:true,
+  slideBy:1,
+  smartSpeed:1000,
+  fluidSpeed:1000,
+  mouseDrag:true,
+  touchDrag:true,
+})
+
+
+});//$(document).ready
+
+
